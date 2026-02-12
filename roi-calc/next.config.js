4bfbe80dfb55cafac718ecc72fb91e96ca576d9f
liulhs <liulhs1998@gmail.com>
2026-02-12 14:12:53 -0800

    refactor: clean up next.config.js by removing unused environment variables

--- roi-calc/next.config.js
+++ roi-calc/next.config.js
@@ -3,8 +3,6 @@
 module.exports = {
   env: {
     REACT_APP_IMAGES_PATH: '/assets/images',
-    NEXTAUTH_SECRET: 'Wxh7ucB6n1ZpL2uSInvk/5Hl5WzgFFuPBhVfy0x6DG0U=',
-    NEXT_PUBLIC_GOOGLE_MAP_API: 'AIzaSyCJM0a8oSaRMwxthozENQg1euRI51aNXJQ',
   },
   images: {
     remotePatterns: [
